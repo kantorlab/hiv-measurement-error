c6d668a8336aac7d7742b2e39da5b6ba4f50777a
Mark Howison <mhowison@brown.edu>
2018-07-03 15:39:10 -0400

    scons: removed unused files

--- lib/model-codons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-PY=$1
-ID=$4
-OUT=$5
-FASTA=scratch/${ID}.collapsed.fa
-N=$(grep -c '^>' $FASTA)
-echo $N
-python $PY $N >$OUT
--- lib/pid-reads.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd scratch
-cat PrimerID/scripts/Comparing\ estimators/3*/3*_R1.fastq >PID_1.fastq
-cat PrimerID/scripts/Comparing\ estimators/3*/3*_R2.fastq >PID_2.fastq
